492f2b9e05e0bb735aa7dc80f22af2770057ec38
Ferformaweb <fernandolagonovoa@gmail.com>
2022-01-24 09:32:11 +0100

    ok

--- build/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "Email: ". $_POST['email']. "<br>";
-echo "Contraseña: ". $_POST['contrasinal']. "<br>";
-echo "Dirección : ". $_POST['direccion']. "<br>";
-echo "Ciudad : ". $_POST['ciudad']. "<br>";
-echo "Provincia: ". $_POST['provincia']. "<br>";
-echo "Comentario: ". $_POST['coments']. "<br>";
-
